36f5109a2486afa287fe17f126619311e4bda6a3
IhorPankivDev <ihorpankiv.dev@gmail.com>
2026-05-11 01:44:44 +0300

    build(client): containerize the React app and wire it into the local compose stack

--- CommentsSPATask.Client/vite.config.ts
+++ CommentsSPATask.Client/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+        allowedHosts: [
+            "74bb-91-194-56-70.ngrok-free.app",
+        ],
+    },
 })
