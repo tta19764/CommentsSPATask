f0a1bb4c47154a88c929ea9c25e27d25972ce3a0
tta19764 <tta19764@gmail.com>
2026-05-12 14:20:35 +0300

    build(deploy): replace railway setup with vm compose deployment

--- CommentsSPATask.Api/Extensions/ServiceCollectionExtensions.cs
+++ CommentsSPATask.Api/Extensions/ServiceCollectionExtensions.cs
@@ -9,18 +9,31 @@ namespace CommentsSPATask.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddApi(this IServiceCollection services)
+    public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCors(options =>
         {
             options.AddPolicy("Client", policy =>
             {
+                var localOrigins = new[]
+                {
+                    "http://localhost:8080",
+                    "http://127.0.0.1:8080",
+                    "http://localhost:5173",
+                    "http://127.0.0.1:5173"
+                };
+
+                var configuredOrigins = configuration
+                    .GetSection("Cors:AllowedOrigins")
+                    .Get<string[]>() ?? [];
+
+                var allowedOrigins = localOrigins
+                    .Concat(configuredOrigins)
+                    .Distinct(StringComparer.OrdinalIgnoreCase)
+                    .ToArray();
+
                 policy
-                    .WithOrigins(
-                        "http://localhost:8080",
-                        "http://127.0.0.1:8080",
-                        "http://localhost:5173",
-                        "http://127.0.0.1:5173")
+                    .WithOrigins(allowedOrigins)
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
